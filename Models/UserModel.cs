b6da4d0ffc1c4d0fc772a8138f23e57c06ed00bf
Ярослав <umpepatopChannel@gmail.com>
2021-09-19 14:29:21 +0500

    Rename db context and cleanup

--- Models/UserModel.cs
+++ Models/UserModel.cs
@@ -19,7 +19,7 @@ namespace EasyPoll.Models
 
         public UserModel() { }
 
-        public UserModel(ViewModels.LoginViewModel viewModel)//, Data.UsersContext usersDbContext)
+        public UserModel(ViewModels.LoginViewModel viewModel)
         {
             UserModel user = GetUserModelByUsername(viewModel.Username);
             bool isPasswordCorrect = new PasswordHasher<UserModel>()
@@ -35,7 +35,6 @@ namespace EasyPoll.Models
             Token = GenerateToken();
             var usersDbContext = GetDBContext();
             usersDbContext.Users.Update(this);
-            //usersDbContext.Users.FromSqlInterpolated($"UPDATE dbo.Users SET Token = {Token} WHERE Id = {Id}");
             usersDbContext.SaveChanges();
         }
 
@@ -56,7 +55,10 @@ namespace EasyPoll.Models
             }
 
             var user = GetUserModelByUsername(username);
-            if (user == null && user.Token != token) return false;
+            if (user == null && user.Token != token)
+            { 
+                return false; 
+            }
             return tokenTime.AddHours(1) > System.DateTime.Now;
         }
 
@@ -66,18 +68,17 @@ namespace EasyPoll.Models
             return usersDbContext.Users.FirstOrDefaultAsync(user => user.Username == username).Result;
         }
 
-        private static UsersContext GetDBContext()
+        private static ServiceDBContext GetDBContext()
         {
-            var optionsBuilder = new DbContextOptionsBuilder<UsersContext>();
-            optionsBuilder.UseSqlServer(AppSettings.Configuration.GetConnectionString("UsersDBContext"));
-            return new UsersContext(optionsBuilder.Options);
+            var optionsBuilder = new DbContextOptionsBuilder<ServiceDBContext>();
+            optionsBuilder.UseSqlServer(AppSettings.Configuration.GetConnectionString("ServiceData"));
+            return new ServiceDBContext(optionsBuilder.Options);
         }
 
         private string GenerateToken()
         {
             string rawToken = $"{System.DateTime.Now.ToBinary()}:{Username}";
-            string token = System.Convert.ToBase64String(
-                    System.Text.Encoding.UTF8.GetBytes(rawToken));
+            string token = System.Convert.ToBase64String(System.Text.Encoding.UTF8.GetBytes(rawToken));
             return token;
         }
 
