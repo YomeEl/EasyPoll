e3d630e51bc2cad822421f9690979a7b95eb876a
Ярослав <umpepatopChannel@gmail.com>
2021-09-26 23:53:07 +0500

    Remove Id

--- Models/AnswerModel.cs
+++ Models/AnswerModel.cs
@@ -2,7 +2,7 @@
 {
     public class AnswerModel
     {
-        public int Id { get; set; }
+        [System.ComponentModel.DataAnnotations.Key]
         public int PollId { get; set; }
         public int UserId { get; set; }
         public int Answer { get; set; }
