486e0c579e23e956d16c12d0e9466d68e074d438
Ярослав <umpepatopChannel@gmail.com>
2021-10-19 13:05:50 +0500

    Add GoBack() method

--- Controllers/SettingsController.cs
+++ Controllers/SettingsController.cs
@@ -9,5 +9,10 @@ namespace EasyPoll.Controllers
         {
             return View();
         }
+
+        public IActionResult GoBack()
+        {
+            return RedirectToAction("ActivePoll", "Poll");
+        }
     }
 }
