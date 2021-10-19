1ce8d993c3f0a6bfbcb5c017372c8ff431601245
Ярослав <umpepatopChannel@gmail.com>
2021-10-19 12:34:40 +0500

    Add SettingsController

---
+++ Controllers/SettingsConroller.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace EasyPoll.Controllers
+{
+    public class SettingsConroller : Controller
+    {
+        public IActionResult ControlPanel()
+        {
+            return View();
+        }
+    }
+}
