5487a8956ea98ffd158a83653266246c42cafcc6
Ярослав <umpepatopChannel@gmail.com>
2021-11-18 19:43:02 +0500

    Move all calculation to controller and use js to display poll

--- Controllers/PollController.cs
+++ Controllers/PollController.cs
@@ -10,29 +10,61 @@ namespace EasyPoll.Controllers
         [HttpGet]
         public IActionResult ActivePoll()
         {
-            if (Request.Query.ContainsKey("answer"))
-            {
-                ViewData["Selected"] = int.Parse(Request.Query["answer"][0]);
-            }
-            else
-            {
-                ViewData["Selected"] = -1;
-            }
-
             if (!HasValidToken())
             {
                 return RedirectToAction("Login", "Authentification");
             }
 
             //TODO: Temporary. Active poll should be static somewhere.
-            var activePoll = Data.ServiceDBContext.GetDBContext().Polls.FirstAsync().Result;
+            var dbcontext = Data.ServiceDBContext.GetDBContext();
+            var activePoll = dbcontext.Polls.FirstAsync().Result;
             ViewData["ActivePoll"] = activePoll;
 
-            var questionList = (from question in Data.ServiceDBContext.GetDBContext().Questions
+            var questions = (from question in dbcontext.Questions
                                 where question.PollId == activePoll.Id
                                 select question).OrderBy(question => question.Id).ToArray();
 
-            ViewData["Questions"] = questionList;
+            var user = Models.UserModel.GetUserByToken(Request.Cookies["token"]);
+            int userId = user.Id;
+            var userSelection = new int[questions.Length];
+            var answers = new int[questions.Length][];
+            for (int i = 0; i < questions.Length; i++)
+            {
+                var qAns = (from answer in dbcontext.Answers
+                            where answer.QuestionId == questions[i].Id
+                            select answer).ToArray();
+                answers[i] = new int[questions[i].Options.Split("~!").Length];
+                foreach (var ans in qAns)
+                {
+                    answers[i][ans.Answer - 1]++;
+                    if (ans.UserId == userId)
+                    {
+                        userSelection[i] = ans.Answer;
+                    }
+                }
+            }
+
+            int totalCount = 0;
+            foreach (var ans in answers[0])
+            {
+                totalCount += ans;
+            }
+
+            var answered = (from answer in dbcontext.Answers
+                           where answer.UserId == userId
+                           select answer).Count() != 0;
+
+            if (answered)
+            {
+                ViewData["Selected"] = 1;
+            }
+
+            ViewData["Answered"] = answered;
+            ViewData["Questions"] = questions;
+            ViewData["Answers"] = answers;
+            ViewData["UserSelection"] = userSelection;
+            ViewData["TotalCount"] = totalCount;
+            ViewData["ShowControlPanelButton"] = user.RoleId > 1;
 
             return View();
         }
@@ -41,7 +73,6 @@ namespace EasyPoll.Controllers
         public IActionResult ActivePoll(int unused)
         {
             var dbcontext = Data.ServiceDBContext.GetDBContext();
-            var activePoll = dbcontext.Polls.FirstAsync().Result;
             var raw_answers = Request.Form["Answers"][0].Split(',');
             for (int i = 0; i < raw_answers.Length; i++)
             {
