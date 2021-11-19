20c6007f11583d46e588ecdb2ee4861912745a4b
Ярослав <umpepatopChannel@gmail.com>
2021-11-19 16:36:16 +0500

    Add Global class initialization

--- Program.cs
+++ Program.cs
@@ -13,7 +13,9 @@ namespace EasyPoll
     {
         public static void Main(string[] args)
         {
-            CreateHostBuilder(args).Build().Run();
+            var host = CreateHostBuilder(args).Build();
+            Global.Initialize();
+            host.Run();
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
