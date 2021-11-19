c6259e83cee66da6d0ec117cd37aea462929106e
Ярослав <umpepatopChannel@gmail.com>
2021-11-19 16:35:31 +0500

    Add static class for global variables

---
+++ Global.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EasyPoll
+{
+    public static class Global
+    {
+        public static Poll ActivePoll { get; set; }
+
+        public static void Initialize()
+        {
+            int activePollId = 1;
+
+            ActivePoll = new Poll(activePollId);
+        }
+    }
+}
