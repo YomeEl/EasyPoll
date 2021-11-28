e041bb852f07aca8a1ab02309cd6593c0b042387
Ярослав <umpepatopChannel@gmail.com>
2021-11-29 03:00:36 +0500

    Make DepartmentId nullable

--- Models/UserModel.cs
+++ Models/UserModel.cs
@@ -19,7 +19,7 @@ namespace EasyPoll.Models
         public string Username { get; set; }
         public string Email { get; set; }
         public int RoleId { get; set; }
-        public int DepartmentId { get; set; }
+        public int? DepartmentId { get; set; }
         public string Key { get; set; }
         public string Token { get; set; }
 
