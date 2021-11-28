738ae5b017a6391d9f02c938c1da1495722464f5
Ярослав <umpepatopChannel@gmail.com>
2021-11-28 17:12:05 +0500

    Add superuser credentials and methods to check them

--- Global.cs
+++ Global.cs
@@ -10,15 +10,46 @@ namespace EasyPoll
         public static Poll ActivePoll { get; set; }
 
         private static int activePollId = 1;
+        private static (string username, string password) superuser;
+        private static string superuserToken;
 
         public static void Initialize()
         {
             UpdateActivePoll();
+            LoadSUData();
         }
 
         public static void UpdateActivePoll()
         {
             ActivePoll = new Poll(activePollId);
+        }
+
+        public static string CheckSUAndGenerateToken(string username, string password)
+        {
+            const int TOKEN_LEN = 40;
+            if ((username, password) == superuser)
+            {
+                var token = "";
+                var rng = new Random();
+                for (int i = 0; i < TOKEN_LEN; i++)
+                {
+                    token += Convert.ToChar(rng.Next(26) + 65);
+                }
+                superuserToken = token;
+                return token;
+            }
+            return "";
+        }
+
+        public static bool CheckSU(string cookie)
+        {
+            return cookie == superuserToken;
+        }
+
+        private static void LoadSUData()
+        {
+            var sr = new System.IO.StreamReader("superuser.txt");
+            superuser = (sr.ReadLine(), sr.ReadLine());
         }
     }
 }
