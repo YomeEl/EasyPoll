7b075b1289eaf2120bda97be2640939906b779e8
Ярослав <umpepatopChannel@gmail.com>
2021-12-01 16:19:04 +0500

    Add OptionModel

---
+++ Models/OptionModel.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace EasyPoll.Models
+{
+    public class OptionModel
+    {
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        [Key]
+        public int Id { get; set; }
+        public int QuestionId { get; set; }
+        public int? MediaId { get; set; }
+        public string Text { get; set; }
+    }
+}
