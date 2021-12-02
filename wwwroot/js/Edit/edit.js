d22fbaef48e2aa654a2bccd8d03c304b24edba97
Ярослав <umpepatopChannel@gmail.com>
2021-12-02 11:40:12 +0500

    Fix quotes

--- wwwroot/js/Edit/edit.js
+++ wwwroot/js/Edit/edit.js
@@ -11,30 +11,30 @@ function init(count) {
 function addOption(val = '') {
     cntr++;
 
-    var newAnswer = document.createElement("input");
+    var newAnswer = document.createElement('input');
     newAnswer.name = 'Answer' + cntr;
-    newAnswer.className = "login-form-input";
-    newAnswer.type = "text";
-    newAnswer.placeholder = "Текст ответа " + cntr;
+    newAnswer.className = 'login-form-input';
+    newAnswer.type = 'text';
+    newAnswer.placeholder = 'Текст ответа ' + cntr;
     newAnswer.value = val;
 
-    var newA = document.createElement("a");
-    newA.href = "";
-    newA.innerText = "Загрузить изображение";
+    var newA = document.createElement('a');
+    newA.href = '';
+    newA.innerText = 'Загрузить изображение';
 
-    var newHr = document.createElement("hr");
+    var newHr = document.createElement('hr');
 
-    var newDiv = document.createElement("div");
-    newDiv.id = "AnswerDiv" + cntr;
+    var newDiv = document.createElement('div');
+    newDiv.id = 'AnswerDiv' + cntr;
     newDiv.appendChild(newAnswer);
     newDiv.appendChild(newA);
     newDiv.appendChild(newHr);
 
-    var ansDiv = document.getElementById("answers");
+    var ansDiv = document.getElementById('answers');
     ansDiv.appendChild(newDiv);
 }
 
 function removeOption() {
-    document.getElementById("answers").removeChild(document.getElementById("AnswerDiv" + cntr));
+    document.getElementById('answers').removeChild(document.getElementById('AnswerDiv' + cntr));
     cntr--;
 }
