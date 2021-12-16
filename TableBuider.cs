cd28218d57727f1514d2fe88df1e720a3dcc58d5
Ярослав <umpepatopChannel@gmail.com>
2021-12-16 16:16:05 +0500

    Add participants sheet to table

--- TableBuider.cs
+++ TableBuider.cs
@@ -11,7 +11,7 @@ namespace EasyPoll
 {
     public class TableBuider
     {
-        private readonly List<string> columns = new() { "Номер ответа", "Ответ", "Подразделение" };
+        private readonly List<string> questionColumns = new() { "Номер ответа", "Ответ", "Подразделение" };
 
         private readonly List<QuestionModel> questions;
 
@@ -34,38 +34,8 @@ namespace EasyPoll
 
         public XSSFWorkbook Build()
         {
-            for (int i = 0; i < questions.Count; i++)
-            {
-                rowIndex = 0;
-
-                var qSheet = workbook.CreateSheet($"Вопрос {i + 1}");
-
-                AddMegedRegion(qSheet, "Текст вопроса", questionTitleStyle);
-                AddMegedRegion(qSheet, questions[i].Question, questionStyle);
-                rowIndex++;
-                AddTableHeader(qSheet);
-
-                var question = questions[i];
-                var answers = ExtractAnswers(question);
-                for (int j = 0; j < answers.Count; j++)
-                {
-                    var answer = answers[j];
-                    var row = qSheet.CreateRow(rowIndex + j + 1);
-                    row.CreateCell(0).SetCellValue(answer.Answer);
-                    row.CreateCell(1).SetCellValue(question.Options[answer.Answer - 1].Text);
-                    row.CreateCell(2).SetCellValue(answer.User.Department.Name);
-                    if (j == answers.Count - 1 || answers[j + 1].Answer != answer.Answer)
-                    {
-                        row.Cells.ForEach(c => c.CellStyle = borderNoTop);
-                    }
-                    else
-                    {
-                        row.Cells.ForEach(c => c.CellStyle = borderSide);
-                    }
-                }
-
-                SetColumnsAutoSize(qSheet);
-            }
+            AddParticipantsSheet();
+            AddQuestionSheets();
 
             return workbook as XSSFWorkbook;
         }
@@ -93,7 +63,85 @@ namespace EasyPoll
             questionStyle.CloneStyleFrom(questionTitleStyle);
             questionStyle.WrapText = true;
         }
-    
+
+        private void AddQuestionSheets()
+        {
+            for (int i = 0; i < questions.Count; i++)
+            {
+                rowIndex = 0;
+
+                var sheet = workbook.CreateSheet($"Вопрос {i + 1}");
+
+                AddMegedRegion(sheet, "Текст вопроса", questionTitleStyle);
+                AddMegedRegion(sheet, questions[i].Question, questionStyle);
+                rowIndex++;
+                AddTableHeader(sheet, questionColumns);
+
+                var question = questions[i];
+                var answers = ExtractAnswers(question);
+                for (int j = 0; j < answers.Count; j++)
+                {
+                    var answer = answers[j];
+                    var row = sheet.CreateRow(rowIndex + j + 1);
+                    row.CreateCell(0).SetCellValue(answer.Answer);
+                    row.CreateCell(1).SetCellValue(question.Options[answer.Answer - 1].Text);
+                    row.CreateCell(2).SetCellValue(answer.User.Department.Name);
+                    if (j == answers.Count - 1 || answers[j + 1].Answer != answer.Answer)
+                    {
+                        row.Cells.ForEach(c => c.CellStyle = borderNoTop);
+                    }
+                    else
+                    {
+                        row.Cells.ForEach(c => c.CellStyle = borderSide);
+                    }
+                }
+
+                SetColumnsAutoSize(sheet);
+            }
+        }
+
+        private void AddParticipantsSheet()
+        {
+            var sheet = workbook.CreateSheet("Участники");
+
+            var answersCnt = new Dictionary<DepartmentModel, int>();
+            questions.ForEach(q => q.Answers.ForEach(a => {
+                int count = q.Answers.Count(ans => ans.User.Department.Id == a.User.Department.Id);
+                answersCnt[a.User.Department] = count;
+            }));
+
+            rowIndex = 0;
+
+            var rowSummary = sheet.CreateRow(rowIndex++);
+            rowSummary.CreateCell(0).SetCellValue("Всего участников");
+            var cellSummary = rowSummary.CreateCell(1);
+            cellSummary.SetCellValue(answersCnt.Values.Sum());
+            cellSummary.SetCellType(CellType.Numeric);
+            rowSummary.Cells.ForEach(c => c.CellStyle = borderFull);
+
+            rowIndex++;
+
+            var rowInfo = sheet.CreateRow(rowIndex);
+            rowInfo.CreateCell(0).SetCellValue("По подразделениям");
+            rowInfo.CreateCell(1);
+            rowInfo.Cells.ForEach(c => c.CellStyle = questionTitleStyle);
+            sheet.AddMergedRegion(new CellRangeAddress(rowIndex, rowIndex, 0, 1));
+            rowIndex++;
+
+            foreach (var dept in answersCnt.Keys.OrderBy(d => d.Id))
+            {
+                var row = sheet.CreateRow(rowIndex++);
+                row.CreateCell(0).SetCellValue(dept.Name);
+                var cell = row.CreateCell(1);
+                cell.SetCellValue(answersCnt[dept]);
+                cell.SetCellType(CellType.Numeric);
+                row.Cells.ForEach(c => c.CellStyle = borderFull);
+            }
+
+            sheet.AutoSizeColumn(0);
+            sheet.AutoSizeColumn(0);
+        }
+
         private void AddMegedRegion(ISheet sheet, string value, ICellStyle style)
         {
             var qRow = sheet.CreateRow(rowIndex);
@@ -107,7 +155,7 @@ namespace EasyPoll
             rowIndex++;
         }
 
-        private void AddTableHeader(ISheet sheet)
+        private void AddTableHeader(ISheet sheet, List<string> columns)
         {
             var row = sheet.CreateRow(rowIndex);
             for (int i = 0; i < columns.Count; i++)
@@ -119,7 +167,7 @@ namespace EasyPoll
 
         private void SetColumnsAutoSize(ISheet sheet)
         {
-            for (int i = 0; i < columns.Count; i++)
+            for (int i = 0; i < questionColumns.Count; i++)
             {
                 sheet.AutoSizeColumn(i);
             }
